47e96472cf86bcf2cc69f54c98d02ca6749e96f6
VinhTin-AQUA <tinhovinh@gmail.com>
2025-07-27 10:41:51 +0700

    feat: download video

---
+++ ExcelVideoLabler.API/Models/ResultDownloadVideo.cs
@@ -0,0 +1,12 @@
+namespace ExcelVideoLabler.API.Models
+{
+    public class ResultDownloadVideo
+    {
+        public bool IsSuccess { get; set; }
+        public string TransId { get; set; } = string.Empty;
+        public string Link { get; set; } = string.Empty;
+        
+        public int TotalDownloadsSuccess { get; set; } 
+        public int TotalDownloadsFailed  { get; set; }
+    }
+}
